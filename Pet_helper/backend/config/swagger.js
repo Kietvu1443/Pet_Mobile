cbce9c6433ae803520ad339a1ebc2f8a0f137ad3
KietVu <tuankiet.vtnktv@gmail.com>
2026-07-27 15:22:44 +0700

    refactor: remove unused files and clean up project structure

--- Pet_helper/backend/config/swagger.js
+++
@@ -1,7 +0,0 @@
-const swaggerUi = require('swagger-ui-express');
-const YAML = require('yamljs');
-const path = require('path');
-
-const swaggerSpec = YAML.load(path.join(__dirname, '../swagger.yaml'));
-
-module.exports = { swaggerUi, swaggerSpec };
